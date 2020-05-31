0bd52c919d4a7efe0b1df5fed4f481681cc46668
Zane Finner <zanefinnergm@gmail.com>
2020-05-31 13:59:01 -0400

    statistics and formatting

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"strings"
+	"time"
 )
 
 func main() {
-	answer := "a"
-	chars := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	tries := 1
+	fmt.Println()
+	start := time.Now()
+	answer := "zane"
+	chars := "`~!@#$%^&*()_+-={[}]\\|;:/?.>,<qazwsxedcrfvtgbyhnujmikolpQAZWSXEDCRFVTGBYHNUJMIKOLP"
+	tries := 0
 	charsSlice := strings.Split(chars, "")
 	var pos []string
 	fmt.Println("Finding all possible combinations...")
@@ -29,7 +32,17 @@ func main() {
 		}
 	}
 	x := whereAt(pos, answer)
-	fmt.Println("Solved on try", x+1, " out of ", tries)
+	elapsed := time.Since(start)
+	comPerSec := float32(tries+1) / float32(elapsed)
+	if x != -1 {
+		fmt.Println("Solved on try", x+1, " out of ", tries)
+		fmt.Println("Stats")
+		fmt.Println("  | Exec Time    : ", elapsed)
+		fmt.Println("  | Sec / Compare: ", comPerSec)
+		fmt.Println()
+	} else {
+		fmt.Print("not found...")
+	}
 }
 func whereAt(a []string, x string) int {
 	for k, n := range a {
